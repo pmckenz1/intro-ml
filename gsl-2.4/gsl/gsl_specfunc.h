63648333a9e557e191bec3000f8a2eb6993a6a1b
pmckenz1 <pmckenz1@utk.edu>
2018-01-16 00:11:31 -0500

    gsl

--- gsl-2.4/gsl/gsl_specfunc.h
+++ gsl-2.4/gsl/gsl_specfunc.h
@@ -1,1 +1,10 @@
-../specfunc/gsl_specfunc.h
+/* Author:  G. Jungman */
+
+        
+/* Convenience header */
+#ifndef __GSL_SPECFUNC_H__
+#define __GSL_SPECFUNC_H__
+
+#include <gsl/gsl_sf.h>
+
+#endif /* __GSL_SPECFUNC_H__ */
